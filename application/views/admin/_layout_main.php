5cfe9bd6dad67356ce4b12277d69a1a37a640b0c
Johnny <jrodeldu@gmail.com>
2013-08-26 18:50:33 +0200

    Bootstrap para el panel de admin.

---
+++ application/views/admin/_layout_main.php
@@ -0,0 +1,44 @@
+<?php $this->load->view('admin/includes/header'); ?>
+
+<body>
+    <nav class="navbar navbar-static-top navbar-inverse" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo site_url('admin/dashboard'); ?>"><?php echo $meta_title; ?></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="<?php echo site_url('admin/dashboard'); ?>">Home</a></li>
+                <li><?php echo anchor('admin/pages', 'pages') ?></li>
+                <li><?php echo anchor('admin/users', 'users') ?></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <!-- Panel principal -->
+            <div class="col-lg-9">
+                <section>
+                    <h2>Page name</h2>
+                </section>
+            </div>
+            <!-- Panel lateral -->
+            <div class="col-lg-3">
+                <section>
+                    <i class="glyphicon glyphicon-user"></i><?php echo mailto('admin@email.com', ' admin@email.com'); ?><br>
+                    <i class="glyphicon glyphicon-off"></i><?php echo anchor('admin/users/logout', ' logout'); ?>
+                </section>
+            </div>
+        </div>
+    </div>
+
+<?php $this->load->view('admin/includes/footer'); ?>
---
+++ application/views/admin/_layout_modal.php
@@ -0,0 +1,23 @@
+<?php $this->load->view('admin/includes/header'); ?>
+
+<body style="background: #555;">
+
+    <div class="modal show">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                    <p>One fine body&hellip;</p>
+                </div>
+                <div class="modal-footer">
+                    &copy; <?php echo $meta_title; ?>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
+
+
+<?php $this->load->view('admin/includes/footer'); ?>
